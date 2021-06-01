f31fcd31a74480cb662a6ba4ae78f994323e050e
Priyadharshini Damodharan <77445249+priyasdamodharan@users.noreply.github.com>
2021-06-01 08:45:46 +0530

    Delete jdbc-rest-demo directory

--- jdbc-rest-demo/src/main/java/com/example/jdbcrestdemo/controller/MovieService.java
+++
@@ -1,9 +0,0 @@
-package com.example.jdbcrestdemo.controller;
-
-public interface MovieService {
-
-    void create(MovieRequest movieRequest);
-    Movie get(String name);
-    void update(String name, MovieRequest movieRequest);
-    void delete(String name);
-}
